3e682c5b23c056c389e2b71c825e24580e71860e
Kira <jimmydelannoy@gmail.com>
2022-01-02 23:47:22 +0100

    add persistent data and Access via a password

---
+++ app.js
@@ -0,0 +1,77 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const fs = require('fs');
+const CronJob = require('cron').CronJob;
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json({ extended: true }));
+
+const getJson = () => {
+    try {
+        const data = fs.readFileSync('data.json', 'utf8');
+        return JSON.parse(data);
+    } catch (err) {
+        console.error(err);
+        return null;
+    }
+};
+
+app.post('/access', (req, res) => {
+    const { password } = req.body;
+    try {
+        const { pass } = getJson();
+        console.log(pass, password, password === pass)
+        res.json({ access : password === pass });
+    } catch (err) {
+        console.error(err);
+        res.status(500);
+    }
+})
+
+app.get('/getZone', (req, res) => {
+    try {
+        const { data } = getJson();
+        res.json(data);
+    } catch (err) {
+        console.error(err);
+        res.status(500);
+    }
+});
+
+app.post('/setZone', (req, res) => {
+    const { coordinate, faction } = req.body;
+
+    try {
+        const file = getJson();
+
+        const newData = file.data.map(coords =>
+            coords.coordinates === coordinate
+                ? { ...coords, faction: faction}
+                : coords
+        );
+
+        fs.writeFile('data.json', JSON.stringify({...file, data: newData}), (err) => {
+            if (err) return console.error(err);
+        });
+    } catch (err) {
+        console.error(err);
+    }
+
+});
+
+
+new CronJob('30 21 12 * * *', async () => {
+
+    fs.copyFile("data.json", `./backup/data_${new Date().getTime()}.json`, (err) => {
+        console.log('copied');
+        if (err) {
+            console.log("Error Found:", err);
+        }
+    });
+}, null, true, 'Europe/Paris');
+
+app.listen(3080, () => {
+    console.log('listening on port 3080');
+})
--- src/App.jsx
+++ src/App.jsx
@@ -10,24 +10,19 @@ import FormControl from '@mui/material/FormControl';
 import Modal from '@mui/material/Modal';
 import Box from '@mui/material/Box';
 import Fade from '@mui/material/Fade';
+import TextField from '@mui/material/TextField';
 
 import { GiWineBottle, GiScorpion } from "react-icons/gi";
 import { BsBoxArrowDown, BsArrowsFullscreen } from "react-icons/bs";
 import { MdCrueltyFree } from "react-icons/md";
 import { CgDanger } from "react-icons/cg";
 import { AiTwotoneHome } from "react-icons/ai";
-
-import {
-    FaReact,
-    FaAnkh,
-    FaHippo,
-    FaEarlybirds,
-    FaPoo,
-    FaRadiation
-} from  "react-icons/fa";
+import { FaReact, FaAnkh, FaHippo, FaEarlybirds, FaPoo, FaRadiation } from  "react-icons/fa";
 
 import { asyncForEach } from './utils/utils';
-import map from './map.png';
+import Map from './pictures/map.png';
+import STALKER_logo from './pictures/STALKER_logo.png';
+import DayZ_Logo from './pictures/DayZ_Logo.png';
 import './App.css';
 
 import Grid from './components/Grid';
@@ -103,6 +98,10 @@ const Image = styled.img `
     height: 100vh;
 `;
 
+const Logo = styled.img `
+    width: 50%;
+`;
+
 const Container = styled.div`
     display: flex;
     height: 100%;
@@ -142,6 +141,60 @@ const Text = styled.span`
     margin-left: 10px;
 `;
 
+const ColorSquare = styled.span`
+    border: 2px solid #1c1c1c;
+    float: left;
+    width: 20px;
+    height: 20px;
+    margin: 5px;
+    border-radius: 3px;
+
+    &.mercenary {
+        background: rgb(0 55 255 / 35%);
+    }
+    &.duty {
+        background: rgb(255 0 0 / 35%);
+    }
+    &.freedom {
+        background: rgb(1 211 8 / 35%);
+    }
+    &.military {
+        background: rgb(38 98 0 / 35%);
+    }
+    &.brotherhood {
+        background: rgb(60 20 0 / 35%);
+    }
+    &.csk {
+        background: rgb(2 138 235 / 35%);
+    }
+    &.ecologist {
+        background: rgb(231 117 0 / 35%);
+    }
+`;
+
+const ListLegend = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    align-content: center;
+    font-weight: 500;
+    font-size: 20px;
+    color: #c1c1c1;
+`;
+
+const ListChild = styled.div`
+    list-style: none;
+`;
+
+const LogoContainer = styled.div `
+    display: flex;
+    flex-direction: row;
+    align-items: flex-end;
+    justify-content: flex-start;
+    align-content: flex-end;
+    height: 100%;
+`;
+
 const x = 9;
 const y = 12;
 
@@ -173,6 +226,10 @@ const factions = [
     {
         name : 'Clear Sky',
         value : 'csk'
+    },
+    {
+        name : 'Ecologiste',
+        value : 'ecologist'
     }
 ];
 
@@ -190,11 +247,18 @@ const styleBox = {
   pb: 3,
 };
 
-function App() {
+const fetchJS = async (url, option = null) => {
+    const result = await fetch(url, option);
+    const json = await result.json();
+    return json;
+}
 
+function App() {
     const [ width, setWidth ] = useState(null);
     const [ height, setHeight ] = useState(null);
     const [ editFaction, setEditFaction] = useState(false);
+    const [ access, setAccess ] = useState(false);
+    const [ passwordVisible, setPasswordVisible] = useState(false);
     const [ selected, setSelected ] = useState(null);
     const [ factionSelected, setFactionSelected ] = useState('');
     const [ coordinates, setCoordinates ] = useState([]);
@@ -202,6 +266,8 @@ function App() {
 
     const [ newAnnotation, setNewAnnotation] = useState(null);
     const [ openModal, setOpenModal ] = useState(false);
+
+    const [ password, setPassword ] = useState('');
 
     const resizeEvent = useCallback(() => {
         const picture = document.getElementById('map');
@@ -219,9 +285,32 @@ function App() {
         resizeEvent();
     }, [ resizeEvent ]);
 
-    const onClick= useCallback(() => {
-        setEditFaction(!editFaction);
-    }, [ editFaction, setEditFaction ]);
+    const onClickAccess = useCallback(async () => {
+        if (password !== '') {
+            const result = await fetchJS('http://localhost:3080/access',
+                {
+                    method: 'POST',
+                    body: JSON.stringify({ password }),
+                    headers: {
+                        'Accept': 'application/json',
+                        'Content-Type': 'application/json'
+                    },
+                    mode: 'cors',
+                    cache: 'default'
+                });
+            if (result.access) {
+                setAccess(true);
+                setPasswordVisible(false);
+            }
+        }
+        setPassword('');
+    }, [ password , setPassword, setAccess, setPasswordVisible ]);
+
+    const onChangePassword = useCallback(event => setPassword(event.target.value), [ setPassword ]);
+
+    const onClick = useCallback(() => {
+        if (access) setEditFaction(!editFaction);
+    }, [ editFaction, setEditFaction, access ]);
 
     const onSelectFaction = useCallback((f) => {
         const oldCoordinates = [ ...coordinates ];
@@ -236,6 +325,24 @@ function App() {
             })
         );
         setCoordinates(tmp);
+
+        const body = {
+            coordinate : selected,
+            faction: f
+        };
+
+        fetchJS('http://localhost:3080/setZone',
+            {
+                method: 'POST',
+                body: JSON.stringify(body),
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                mode: 'cors',
+                cache: 'default'
+            }
+        );
     }, [ coordinates, setCoordinates, selected ]);
 
     const onChange = useCallback(event => {
@@ -283,6 +390,10 @@ function App() {
         setOpenModal(false);
     }, [ setOpenModal ]);
 
+    const onhandleClosePasswordModal = useCallback(event => {
+        setPasswordVisible(false);
+    }, [ setPasswordVisible ]);
+
     const onClickContainer = useCallback(event => {
         if (!editFaction) {
             const rect = event.target.getBoundingClientRect();
@@ -302,17 +413,21 @@ function App() {
         yArray.shift();
 
         const init = async () => {
+            const data = await fetchJS('http://localhost:3080/getZone');
             let tmp = [ ];
             await asyncForEach(xArray, async xValue => {
                 const charX = String.fromCharCode(64 + xValue);
                 const arrayX = [];
                 await asyncForEach(yArray, async (yValue, index) => {
                     const coordinatesXY = `${charX}${yValue}`;
+
+                    const coordinatesFetched = data.find(coord => coord.coordinates === coordinatesXY);
+
                     arrayX.push({
                         coordinates : coordinatesXY,
                         x: charX,
                         y: yValue,
-                        faction: '',
+                        faction: coordinatesFetched ? coordinatesFetched.faction : '',
                         selected: false
                     })
                 });
@@ -321,44 +436,82 @@ function App() {
             setCoordinates(tmp);
         };
         init();
+
+        window.addEventListener('keydown', event => {
+            const { shiftKey, ctrlKey, key } = event;
+            if (shiftKey && ctrlKey && key === 'K') {
+                if (access) {
+                    setAccess(false);
+                }else{
+                    setPasswordVisible(true);
+                }
+                event.stopPropagation();
+            }
+        });
+
     }, [ setCoordinates ]);
+
+
 
     return (
         <Context.Provider value={providerValue}>
             <Wrapper>
                 <NavBar>
-                    <ButtonEdit
-                        variant="contained"
-                        color="secondary"
-                        onClick={onClick}
-                    >
-                        {editFaction ? 'Bloquer Factions' : 'Modifier Factions'}
-                    </ButtonEdit>
-
-                    <CardPerso>
-                        <CardHeader
-                            title="Faction"
-                            subheader="Choisis la faction qui gère cette zone"
-                        />
-                        <CardContent>
-                            <FormControl sx={{ width: '90%'  }} disabled={!editFaction}>
-                                <Select
-                                    className={!editFaction ? 'Mui-disabled' : ''}
-                                    id="select-faction"
-                                    value={factionSelected}
-                                    label="Factions"
-                                    onChange={onChange}
-                                    disabled={!editFaction}
+                    {
+                        access && (
+                                <ButtonEdit
+                                    variant="contained"
+                                    color="secondary"
+                                    onClick={onClick}
                                 >
-                                    {
-                                        factions.map(faction =>
-                                            <MenuItem key={uuid()} value={faction.value}>{faction.name}</MenuItem>
-                                        )
-                                    }
-                                </Select>
-                            </FormControl>
-                        </CardContent>
-                    </CardPerso>
+                                    {editFaction ? 'Bloquer Factions' : 'Modifier Factions'}
+                                </ButtonEdit>
+                        )
+                    }
+                    {
+                        access && (
+                                <CardPerso sx={{ minHeight: '200px', height: '200px', maxHeight: '200px' }}>
+                                    <CardHeader
+                                        title="Faction"
+                                        subheader="Choisis la faction qui gère cette zone"
+                                    />
+                                    <CardContent>
+                                        <FormControl sx={{ width: '90%' }} disabled={!editFaction}>
+                                            <Select
+                                                className={!editFaction ? 'Mui-disabled' : ''}
+                                                id="select-faction"
+                                                variant="outlined"
+                                                value={factionSelected}
+                                                label="Factions"
+                                                onChange={onChange}
+                                                disabled={!editFaction}
+                                            >
+                                                {
+                                                    factions.map(faction =>
+                                                        <MenuItem key={uuid()} value={faction.value}>{faction.name}</MenuItem>
+                                                    )
+                                                }
+                                            </Select>
+                                        </FormControl>
+                                    </CardContent>
+                                </CardPerso>
+                        )
+                    }
+                    <ListLegend>
+                        {
+                            factions.map(faction =>
+                                <ListChild key={uuid()}>
+                                    <ColorSquare className={faction.value}></ColorSquare>
+                                    {faction.name}
+
+                                </ListChild>
+                            )
+                        }
+                    </ListLegend>
+                    <LogoContainer>
+                        <Logo src={STALKER_logo} alt="logoStalker" />
+                        <Logo src={DayZ_Logo} alt="logoDayz" />
+                    </LogoContainer>
                 </NavBar>
 
                 <Container onClick={onClickContainer} className='map-container'>
@@ -378,7 +531,7 @@ function App() {
                         maxWidth={width}
                         maxHeight={height}
                     />
-                    <Image src={map} alt='map' id='map'/>
+                    <Image src={Map} alt='map' id='map'/>
                 </Container>
             </Wrapper>
 
@@ -390,7 +543,7 @@ function App() {
             >
                 <Fade in={openModal}>
                     <Box sx={{ ...styleBox, width: 500 }}>
-                        <h2 id="parent-modal-title">Text in a modal</h2>
+                        <h2 id="parent-modal-title">Ajouter une annotation</h2>
                             <FormControl sx={{ width: '90%' }}>
                                 <Select
                                     id="select-faction"
@@ -411,6 +564,37 @@ function App() {
                 </Fade>
             </Modal>
 
+            <Modal
+              open={passwordVisible}
+              onClose={onhandleClosePasswordModal}
+              aria-labelledby="edit-faction-modal-title"
+              aria-describedby="edit-faction-modal-description"
+            >
+                <Fade in={passwordVisible}>
+                    <Box sx={{ ...styleBox, width: 500 }}>
+                        <h2 id="edit-faction- parent-modal-title">Accès à la modification des zones</h2>
+                            <FormControl sx={{ width: '90%' }}>
+                                <TextField
+                                    id="outlined-basic"
+                                    variant="outlined"
+                                    label="Mot de passe"
+                                    type="password"
+                                    autoComplete="current-password"
+                                    value={password}
+                                    onChange={onChangePassword}
+                                />
+                                <ButtonEdit
+                                    variant="contained"
+                                    color="secondary"
+                                    onClick={onClickAccess}
+                                    sx={{ margin: '10px 0' }}
+                                >
+                                    Débloquer
+                                </ButtonEdit>
+                            </FormControl>
+                    </Box>
+                </Fade>
+            </Modal>
         </Context.Provider>
     );
 }
